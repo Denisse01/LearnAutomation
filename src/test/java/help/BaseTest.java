fdaa8360cb3af02b2b590a6fafca40fe4246ffe6
Denisse01 <53311258+Denisse01@users.noreply.github.com>
2019-07-26 18:56:45 +0300

    refactoring the project

---
+++ src/test/java/help/BaseTest.java
@@ -0,0 +1,57 @@
+package help;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    public static Properties property;
+    public FileInputStream file;
+
+    @Before
+    public void setup() throws FileNotFoundException {
+        System.setProperty("webdriver.gecko.driver","C:\\Automation\\FirefoxDriver\\geckodriver.exe");
+        driver=new FirefoxDriver();
+        driver.get("http://demo.automationtesting.in/Register.html");
+        driver.manage().window().maximize();
+
+        //        Wait implicit
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loadproperty();
+    }
+
+    //    metoda care iti incarce o fisier de proprietati
+
+    public void loadproperty () throws FileNotFoundException {
+        property = new Properties();
+        file = new FileInputStream("C:\\Users\\Bogar\\IdeaProjects\\probaAutomation\\src\\test\\resources\\InputData.properties");
+        try {
+            property.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+//  metoda care returneaza valoare dint-un fis.prop. pe baza unui Key
+
+    public static String getvalue (String key){
+
+        return property.getProperty(key);
+    }
+
+//    @After
+//    public void inchide (){
+//        driver.quit();
+//    }
+}
